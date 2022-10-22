7d044174026128d4dcdf6b09fa7d56caf62f6712
molychn <ssomkhyy@gmail.com>
2022-10-22 23:15:28 +0800

    测试环境参数

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const argv = process.argv;
 const args = argv.slice(2);
 
 console.log(args);
-request(args[0], {
-  method: 'POST',
-  headers: {
-    cookie: `sessionid=${args[1]}`
-  }
-}, (err, res, body) => {
-  console.log(body);
-})
+// request(args[0], {
+//   method: 'POST',
+//   headers: {
+//     cookie: `sessionid=${args[1]}`
+//   }
+// }, (err, res, body) => {
+//   console.log(body);
+// })
