55264cc79c30308e70463e6bcc7f02bfab73af48
molychn <ssomkhyy@gmail.com>
2022-10-22 23:02:10 +0800

    自动化脚本

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const request = require('request');
+const argv = process.argv;
+const args = argv.slice(2);
+
+request(args[0], {
+  method: 'POST',
+  headers: {
+    cookie: `sessionid=${args[1]}`
+  }
+}, (err, res, body) => {
+  console.log(body);
+})
