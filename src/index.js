adaf47295cba49b13d4b5815eeeb35a04de03d52
molychn <ssomkhyy@gmail.com>
2022-10-24 21:31:15 +0800

    增加自动抽奖

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ const request = require('request');
 const argv = process.argv;
 const args = argv.slice(2);
 
-const url = 'https://api.juejin.cn/growth_api/v1/check_in'
-const reqUrl = `${url}?aid${args[0]}&uuid=${args[1]}&spider=${args[2]}&_signature=${args[3]}`
-request(reqUrl, {
+const url = 'https://api.juejin.cn/growth_api/v1/check_in';
+const checkInUrl = `${url}?aid${args[0]}&uuid=${args[1]}&spider=${args[2]}&_signature=${args[3]}`;
+request(checkInUrl, {
+  method: 'POST',
+  headers: {
+    cookie: `sessionid=${args[4]}`
+  }
+}, (err, res, body) => {
+  console.log(body);
+  checkInRet(body);
+})
+const drawUrl = `https://api.juejin.cn/growth_api/v1/lottery/draw?aid${args[0]}&uuid=${args[1]}&spider=${args[2]}&_signature=${args[3]}`;
+request(drawUrl, {
   method: 'POST',
   headers: {
     cookie: `sessionid=${args[4]}`
